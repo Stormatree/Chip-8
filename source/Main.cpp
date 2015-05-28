1b0265ed8593ec0c55969a8a2e21bb0d24654a6d
Elliott Slingsby <elliottslingsby@googlemail.com>
2015-05-28 03:12:16 +0100

    Initial commit

---
+++ source/Main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+
+#undef main
+
+int main(int argc, char* argv[]){
+	printf("Hello World!\n");
+
+	getchar();
+	return 0;
+}
